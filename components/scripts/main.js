b018a7920016bb77e6c7050fefeb97f25117ec8d
Tim Cartwright <tim@websitesbytim.com>
2015-10-27 16:38:11 +0530

    Line up scroll with beginning of section

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	    var $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top - 70
+	        'scrollTop': $target.offset().top - 50
 	    }, 900, 'swing', function () {
 	        window.location.hash = target;
 	    });
